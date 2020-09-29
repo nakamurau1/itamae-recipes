7efb4fddc80c10c67aaa24726a4fab18190517a4
Yuichi Nakamura <yuichi.nakamura@jmty.jp>
2020-09-29 18:02:21 +0900

    設定ファイルの文法がおかしかったのでnginxが起動しなかった

--- roles/web.rb
+++ roles/web.rb
@@ -1,4 +1,4 @@
 include_recipe "../cookbooks/rbenv/default.rb"
 include_recipe "../cookbooks/nodejs/default.rb"
-include_recipe "../cookbooks/nginx/default.rb"
 include_recipe "../cookbooks/rails/default.rb"
+include_recipe "../cookbooks/nginx/default.rb"
