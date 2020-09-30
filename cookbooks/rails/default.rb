4f213d867654bff65b29de900a88c0fb4253e12d
Yuichi Nakamura <yuichi.nakamura@jmty.jp>
2020-09-30 17:34:01 +0900

    環境変数の設定ファイルが読み込めなかったのでRailsを起動できなかった問題の対応

--- cookbooks/rails/default.rb
+++ cookbooks/rails/default.rb
@@ -11,7 +11,8 @@ execute "export RAILS_MASTER_KEY=51e9c389e374b5e90cd353e6e450bbd3" do
   user "ec2-user"
 end
 
-execute "cd #{HOME_DIR}/itamae-sample; #{RBENV_PATH}/rbenv exec bundle exec unicorn_rails -c config/unicorn.rb -E development -D" do
-  not_if "test -f /tmp/unicorn.pid"
+execute "source /home/ec2-user/.bashrc && #{RBENV_PATH}/rbenv exec bundle exec unicorn_rails -c config/unicorn.rb -E development -D;" do
+  not_if "test -f unicorn.pid"
+  cwd "#{HOME_DIR}/itamae-sample"
   user "ec2-user"
 end
